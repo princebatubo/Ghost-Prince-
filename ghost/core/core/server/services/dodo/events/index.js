99dbebf54b1a6795b25e7c84d890c0b0bc4c0f4d
princebatubo <pdawaribatubo@gmail.com>
2025-07-24 19:28:31 +0100

    Update index.js
    chore(dodo): add index export for Dodo Payments event classes
    
    - Adds DodoLiveEnabledEvent and DodoLiveDisabledEvent exports
    - Provides a single import point for Dodo Payments event classes
    - Mirrors structure of Stripe event index for consistency

--- ghost/core/core/server/services/dodo/events/index.js
+++ ghost/core/core/server/services/dodo/events/index.js
@@ -1,1 +1,15 @@
+/**
+ * Dodo Payments Event Index
+ * 
+ * This index file aggregates and exports all event classes 
+ * related to Dodo Payments webhook system.
+ * 
+ * Currently includes:
+ * - DodoLiveEnabledEvent: Represents when Dodo Payments live mode is enabled
+ * - DodoLiveDisabledEvent: Represents when Dodo Payments live mode is disabled
+ */
 
+module.exports = {
+    DodoLiveEnabledEvent: require('./DodoLiveEnabledEvent'),
+    DodoLiveDisabledEvent: require('./DodoLiveDisabledEvent')
+};
